03ea51542b0b75e8a09331686c8bbb21d17ae5c7
Paul <smartwp@126.com>
2016-09-12 17:59:25 +0800

    客户端连接处理优化

--- client/client.go
+++ client/client.go
@@ -10,30 +10,35 @@ import (
 type Handler func(*Context)
 
 type Client struct {
+	conn           net.Conn
 	packet         chan linker.Packet
 	receivePackets map[uint32]linker.Packet
 	protocolPacket linker.Packet
 }
 
-func NewClient() *Client {
-	return &Client{
+func NewClient(network, address string) *Client {
+	client := &Client{
 		packet:         make(chan linker.Packet, 100),
 		receivePackets: make(map[uint32]linker.Packet, 100),
 	}
-}
 
-func (c *Client) Run(network, address string) {
 	conn, err := net.Dial(network, address)
 	if err != nil {
 		panic("start client:" + err.Error())
 	}
-	defer conn.Close()
 
-	c.handleConnection(conn)
+	client.conn = conn
+	go client.handleConnection(client.conn)
+
+	return client
 }
 
 func (c *Client) SetProtocolPacket(packet linker.Packet) {
 	c.protocolPacket = packet
+}
+
+func (c *Client) Close() error {
+	return c.conn.Close()
 }
 
 func (c *Client) SyncCall(operator string, pb interface{}, response func(*Context)) error {

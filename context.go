3b704bd0bcb2adc05e5f67d15d15309c1b9c7702
Paul <smartwp@126.com>
2016-10-07 22:46:09 +0800

    添加成功和失败的方法

--- context.go
+++ context.go
@@ -1,11 +1,14 @@
 package linker
 
-import "context"
+import (
+	"context"
+	"hash/crc32"
+)
 
 type Context struct {
 	context.Context
-	Request  *Request
-	Response Response
+	request  *Request
+	response Response
 }
 
 func NewContext(ctx context.Context, req *Request, res Response) *Context {
@@ -17,14 +20,26 @@ func NewContext(ctx context.Context, req *Request, res Response) *Context {
 }
 
 func (c *Context) ParseParam(data interface{}) error {
-	return c.Request.Params.UnPack(data)
+	return c.request.Params.UnPack(data)
 }
 
-func (c *Context) Write(operator uint32, data interface{}) (int, error) {
-	p, err := c.Request.Params.Pack(operator, data)
+func (ctx *Context) Success(data interface{}) {
+	ctx.write(ctx.request.Method, data)
+}
+
+func (ctx *Context) Error(data interface{}) {
+	ctx.write(uint32(0), data)
+}
+
+func (c *Context) Write(operator string, data interface{}) (int, error) {
+	return c.write(crc32.ChecksumIEEE([]byte(operator)), data)
+}
+
+func (c *Context) write(operator uint32, data interface{}) (int, error) {
+	p, err := c.request.Params.Pack(operator, data)
 	if err != nil {
 		return 0, err
 	}
 
-	return c.Response.Write(p.Bytes())
+	return c.response.Write(p.Bytes())
 }

1c01a3624d2d5599ce1ca1b0c775b0926c3bb8a4
Paul <smartwp@126.com>
2017-01-08 12:32:19 +0800

    添加版本信息

--- constant.go
+++ constant.go
@@ -1,5 +1,7 @@
 package linker
 
+import "runtime"
+
 const (
 	MaxPayload = 2048
 )
@@ -7,4 +9,10 @@ const (
 const (
 	OPERATOR_HEARTBEAT = iota
 	MAX_OPERATOR       = 1024
+)
+
+const (
+	LINKER_VERSION = "1.0"
+	LINKER_OS      = runtime.GOOS
+	LINKER_ARCH    = runtime.GOARCH
 )

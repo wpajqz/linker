cee2d97d7ab2fcf7f65d368ed7f8e30f7b22c8f2
Paul <smartwp@126.com>
2017-01-17 23:39:07 +0800

    删除未使用内容

--- constant.go
+++ constant.go
@@ -1,7 +1,5 @@
 package linker
 
-import "runtime"
-
 const (
 	MaxPayload = 2048
 )
@@ -9,10 +7,4 @@ const (
 const (
 	OPERATOR_HEARTBEAT = iota
 	MAX_OPERATOR       = 1024
-)
-
-const (
-	LINKER_VERSION = "1.0"
-	LINKER_OS      = runtime.GOOS
-	LINKER_ARCH    = runtime.GOARCH
 )

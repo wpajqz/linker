64a8ff13a69da693a640cf0579a997d7edb63078
Paul <smartwp@126.com>
2017-07-10 23:15:58 +0800

    添加常量定义

--- constant.go
+++ constant.go
@@ -8,3 +8,11 @@ const (
 	OPERATOR_HEARTBEAT = iota
 	MAX_OPERATOR       = 1024
 )
+
+const (
+	// Version current Linker version
+	VERSION = "1.0.0"
+
+	// MinimumGoVersion minimum required Go version for Linker
+	MINIMUM_GO_VERSION = ">=1.9"
+)

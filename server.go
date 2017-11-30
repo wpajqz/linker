597b3dd8ce9c875cb4052257941c30aeade3461c
Paul <smartwp@126.com>
2017-11-30 18:23:29 +0800

    private code move to end

--- server.go
+++ server.go
@@ -83,6 +83,58 @@ func (s *Server) Run(name, address string) (err error) {
 	return err
 }
 
+// 在服务中注册要处理的handler
+func (s *Server) Handle(pattern string, handler Handler) {
+	data := []byte(pattern)
+	operator := crc32.ChecksumIEEE(data)
+
+	if _, ok := s.handlerContainer[operator]; !ok {
+		s.handlerContainer[operator] = handler
+	}
+}
+
+// 绑定Server需要处理的router
+func (s *Server) BindRouter(routers []Router) {
+	for _, router := range routers {
+		operator := crc32.ChecksumIEEE([]byte(router.Operator))
+		if operator <= OPERATOR_MAX {
+			panic("Unavailable operator, the value of crc32 need less than " + strconv.Itoa(OPERATOR_MAX))
+		}
+
+		for _, m := range router.Middleware {
+			s.routerMiddleware[operator] = append(s.routerMiddleware[operator], m)
+		}
+
+		s.Handle(router.Operator, router.Handler)
+	}
+}
+
+// 添加请求需要进行处理的中间件
+func (s *Server) Use(middleware ...Middleware) {
+	s.middleware = append(s.middleware, middleware...)
+}
+
+// 设置默认错误处理方法
+func (s *Server) OnError(errorHandler ErrorHandler) {
+	s.errorHandler = errorHandler
+}
+
+// 客户端链接断开以后执行回收操作
+func (s *Server) OnClose(handler Handler) {
+	s.destructHandler = handler
+}
+
+// 客户端建立连接以后初始化操作
+func (s *Server) OnOpen(handler Handler) {
+	s.constructHandler = handler
+}
+
+// 设置心跳包的handler,需要客户端发送心跳包才能够触发
+// 客户端发送心跳包，服务端未调用此方法时只起到建立长连接的作用
+func (s *Server) OnPing(handler Handler) {
+	s.handlerContainer[OPERATOR_HEARTBEAT] = handler
+}
+
 // 开始运行Tcp服务
 func (s *Server) runTcp(name, address string) error {
 	listener, err := net.Listen(name, address)
@@ -171,56 +223,4 @@ func (s *Server) runWebSocket(name, address string) error {
 	fmt.Printf("%s server running on %s\n", name, address)
 
 	return http.ListenAndServe(address, nil)
-}
-
-// 在服务中注册要处理的handler
-func (s *Server) Handle(pattern string, handler Handler) {
-	data := []byte(pattern)
-	operator := crc32.ChecksumIEEE(data)
-
-	if _, ok := s.handlerContainer[operator]; !ok {
-		s.handlerContainer[operator] = handler
-	}
-}
-
-// 绑定Server需要处理的router
-func (s *Server) BindRouter(routers []Router) {
-	for _, router := range routers {
-		operator := crc32.ChecksumIEEE([]byte(router.Operator))
-		if operator <= OPERATOR_MAX {
-			panic("Unavailable operator, the value of crc32 need less than " + strconv.Itoa(OPERATOR_MAX))
-		}
-
-		for _, m := range router.Middleware {
-			s.routerMiddleware[operator] = append(s.routerMiddleware[operator], m)
-		}
-
-		s.Handle(router.Operator, router.Handler)
-	}
-}
-
-// 添加请求需要进行处理的中间件
-func (s *Server) Use(middleware ...Middleware) {
-	s.middleware = append(s.middleware, middleware...)
-}
-
-// 设置默认错误处理方法
-func (s *Server) OnError(errorHandler ErrorHandler) {
-	s.errorHandler = errorHandler
-}
-
-// 客户端链接断开以后执行回收操作
-func (s *Server) OnClose(handler Handler) {
-	s.destructHandler = handler
-}
-
-// 客户端建立连接以后初始化操作
-func (s *Server) OnOpen(handler Handler) {
-	s.constructHandler = handler
-}
-
-// 设置心跳包的handler,需要客户端发送心跳包才能够触发
-// 客户端发送心跳包，服务端未调用此方法时只起到建立长连接的作用
-func (s *Server) OnPing(handler Handler) {
-	s.handlerContainer[OPERATOR_HEARTBEAT] = handler
 }
